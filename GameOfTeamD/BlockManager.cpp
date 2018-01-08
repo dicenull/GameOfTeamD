a6f136c833a20ae3ab23ff0de6483c47381d87a2
dicen <dicenull.dev@gmail.com>
2018-01-08 12:18:42 +0900

    ブロックを管理するクラスを実装

--- GameOfTeamD/BlockManager.cpp
+++ GameOfTeamD/BlockManager.cpp
@@ -2,11 +2,55 @@
 
 
 
-BlockManager::BlockManager()
+BlockManager::BlockManager(int32 zk)
 {
+	m_p1_blocks.clear();
+	m_p2_blocks.clear();
+	m_zk = zk;
+	m_window_width = Window::Width();
 }
-
 
 BlockManager::~BlockManager()
 {
+}
+
+void BlockManager::CreateBlock(Players p, int height, Block block)
+{
+	switch (p)
+	{
+	case Players::One:
+		block.SetPos(Point(m_window_width - m_zk, height * m_zk));
+		m_p1_blocks.push_back(block);
+		break;
+	case Players::Two:
+		block.SetPos(Point(m_zk, height * m_zk));
+		m_p2_blocks.push_back(block);
+		break;
+	}
+}
+
+void BlockManager::Update()
+{
+	for (Block& block : m_p1_blocks)
+	{
+		block.Move(One);
+	}
+
+	for (Block& block : m_p2_blocks)
+	{
+		block.Move(Two);
+	}
+}
+
+void BlockManager::DrawBlocks() const
+{
+	for (auto block : m_p1_blocks)
+	{
+		block.Draw(m_zk);
+	}
+
+	for (auto block : m_p2_blocks)
+	{
+		block.Draw(m_zk);
+	}
 }
--- GameOfTeamD/BlockManager.h
+++ GameOfTeamD/BlockManager.h
@@ -1,8 +1,24 @@
 #pragma once
+#include <Siv3D.hpp>
+#include "Block.h"
+#include "PlayerLib.h"
+#include "Field.h"
+
 class BlockManager
 {
 public:
-	BlockManager();
+	BlockManager(int32 zk);
 	~BlockManager();
+
+public:
+	void CreateBlock(Players p, int height, Block block);
+	void Update();
+	void DrawBlocks() const;
+
+private:
+	Array<Block> m_p1_blocks;
+	Array<Block> m_p2_blocks;
+	int32 m_zk;
+	int32 m_window_width;
 };
 
