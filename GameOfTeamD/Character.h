a8dbf3d020d70b9b24e45e93fe65390aedd50b66
dice <dicenull.dev@gmail.com>
2018-01-14 19:28:46 +0900

    コメントを追加

--- GameOfTeamD/Character.h
+++ GameOfTeamD/Character.h
@@ -6,16 +6,33 @@ template <class Type>
 class Character
 {
 public:
+	///<summary>
+	/// コンストラクタ
+	///</summary>
+	///<param name="speed">キャラクタの動く速さ</param>
 	Character(double speed = 2.0);
+
+	///<summary>
+	/// デストラクタ
+	///</summary>
 	virtual ~Character();
 
 public:
+	///<summary>
+	/// スピードを取得する
+	///</summary>
 	double Speed();
 
 public:
+	///<summary>
+	/// キャラクタの図形を取得する
+	///</summary>
 	Type Shape;
 
 private:
+	///<summary>
+	/// 速さのデータ
+	///</summary>
 	double m_speed;
 };
 
