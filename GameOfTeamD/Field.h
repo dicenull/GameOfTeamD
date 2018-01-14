8f08b07a213c8ff414fc20b54845a9092469b8cc
dice <dicenull.dev@gmail.com>
2018-01-14 19:51:08 +0900

    Fieldの関数にコメントを追加

--- GameOfTeamD/Field.h
+++ GameOfTeamD/Field.h
@@ -8,49 +8,177 @@
 class Field
 {
 public:
+	///<summary>
+	/// コンストラクタ
+	///</summary>
+	///<param name="field_heigt">フィールドの高さ[zk]</param>
+	///<param name="puzzle_width">パズルフィールドの幅[zk]</param>
+	///<param name="player_width">プレイヤーフィールドの幅[zk]</param>
+	///<param name="zk_size">1マス当たりの大きさ</param>
+	///<param name="is_mirrors">各プレイヤーのフィールドを反転するか</param>
 	Field(int32 field_height = 8, int32 puzzle_width = 10, int32 player_width = 3, int32 zk_size = 25,
 		std::map<PlayerType, bool> is_mirrors = { {PlayerType::One, false}, {PlayerType::Two, true } });
+
+	///<summary>
+	/// コンストラクタ
+	///</summary>
 	Field(const Field& other);
 	void operator=(const Field& other);
+
+	///<summary>
+	/// デストラクタ
+	///</summary>
 	~Field();
 
 public:
+	///<summary>
+	/// フィールドを描画する
+	///</summary>
 	void Draw() const;
+
+	///<summary>
+	/// フィールドの状態を更新する
+	///</summary>
 	void Update();
 
+	///<summary>
+	/// フィールドの高さを取得する
+	///</summary>
 	int32 Height() const;
+
+	///<summary>
+	/// パズルフィールドとプレイヤーフィールドの幅を合わせた大きさを取得する
+	///</summary>
 	int32 Width() const;
+
+	///<summary>
+	/// パズルフィールドの幅を取得する
+	///</summary>
 	int32 PuzzleWidth() const;
+
+	///<summary>
+	/// プレイヤーフィールドの幅を取得する
+	///</summary>
 	int32 PlayerWidth() const;
+
+	///<summary>
+	/// ブロックが移動するフィールドの幅を取得する
+	///</summary>
 	int32 SpaceWidth() const;
+
+	///<summary>
+	/// 1マス当たりの大きさを取得する
+	///</summary>
 	int32 Zk() const;
 
+	///<summary>
+	/// パズルフィールドの左上の座標
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
 	Point PuzzleOrigin(PlayerType p) const;
+	///<summary>
+	/// プレイヤーフィールドの左上の座標
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
 	Point PlayerOrigin(PlayerType p) const;
+	///<summary>
+	/// ブロックが移動するフィールドの左上の座標
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
 	Point SpaceOrigin(PlayerType p) const;
 
+	///<summary>
+	/// パズルフィールドの右下の座標
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
 	Point PuzzleEndPos(PlayerType p) const;
+	///<summary>
+	/// プレイヤーフィールドの右下の座標
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
 	Point PlayerEndPos(PlayerType p) const;
+	///<summary>
+	/// ブロックが移動するフィールドの右下の座標
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
 	Point SpaceEndPos(PlayerType p) const;
 
+	///<summary>
+	/// フィールドのスコアボードとの境界である左下の座標
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
 	Point LeftBorder(PlayerType p) const;
+
+	///<summary>
+	/// フィールドのスコアボードとの境界である右下の座標
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
 	Point RightBorder(PlayerType p) const;
 
+	///<summary>
+	/// プレイヤーフィールドのSpaceフィールドの境界の上の座標
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
 	Point PlayerTopBorder(PlayerType p) const;
 
+	///<summary>
+	/// プレイヤーフィールドの中点
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
 	Point PlayerCenter(PlayerType p) const;
 
+	///<summary>
+	/// ブロックが移動するフィールドの端、左上の座標
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
 	Point BlockStartPos(PlayerType p) const;
 
+	///<summary>
+	/// フィールドを反転するか設定する
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
+	///<param name="is_mirror">反転するか</param>
 	void SetMirror(PlayerType p, bool is_mirror);
 
+	///<summary>
+	/// ブロックが進むべき方向を取得する
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
 	Action FieldDirection(PlayerType p) const;
 
+	///<summary>
+	/// パズルフィールド内にposがあるか
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
+	///<param name="pos>判定する座標</param>
 	bool IsInPuzzleField(PlayerType p, Point pos) const;
+
+	///<summary>
+	/// プレイヤーフィールド内にposがあるか
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
+	///<param name="pos>判定する座標</param>
 	bool IsInPlayerField(PlayerType p, Point pos) const;
+
+	///<summary>
+	/// ブロックが移動するフィールド内にposがあるか
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
+	///<param name="pos>判定する座標</param>
 	bool IsInSpaceField(PlayerType p, Point pos) const;
+
+	///<summary>
+	/// フィールド内にposがあるか
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
+	///<param name="pos>判定する座標</param>
 	bool IsInField(PlayerType p, Point pos) const;
 
+	///<summary>
+	/// パズルフィールド内にshapeがあるか
+	///</summary>
+	///<param name="p">どのプレイヤーがいるフィールドか</param>
+	///<param name="pos>判定する図形</param>
 	template <class Type>
 		bool IsInPlayerField(PlayerType p, Type shape) const;
 
@@ -61,6 +189,9 @@ public:
 		bool IsInBlockField(PlayerType p, Type shape) const;
 
 private:
+	///<summary>
+	/// 画面の大きさのデータ
+	///</summary>
 	Point m_window = Window::Size();
 
 	/// <summary>
@@ -83,9 +214,19 @@ private:
 	/// </summary>
 	int32 m_zk;
 
+	///<summary>
+	/// プレイヤー1のパズルリスト
+	///</summary>
 	Grid<Rect> m_p1_puzzles;
+
+	///<summary>
+	/// プレイヤー2のパズルリスト
+	///</summary>
 	Grid<Rect> m_p2_puzzles;
 	
+	///<summary>
+	/// 各プレイヤーのフィールドが反転しているか管理する
+	///</summary>
 	std::map<PlayerType, bool> m_is_mirror;
 };
 
